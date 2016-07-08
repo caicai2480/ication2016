b252cfb73029b2db1fc78c11924b050ae3e07866
caicai2480 <540247388@qq.com>
2016-07-08 17:40:09 +0800

    Initial commit

---
+++ app/src/main/java/com/example/cai/myapplication/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.cai.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ListView listView;
+    String [] str={"123","3232","0","123"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        listView = (ListView) findViewById(R.id.listview);
+        ArrayAdapter adapter= new ArrayAdapter(this,android.R.layout.simple_list_item_1,str);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+        });
+
+    }
+}
